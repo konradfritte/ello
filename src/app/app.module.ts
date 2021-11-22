67790fa2a250a279926809b9a26ea7aa22c7e81d
Konrad Fritsche <konrad.frit@gmail.com>
2021-11-22 19:32:59 +0100

    master | preview angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabNavigationComponent } from './components/tab-navigation/tab-navigation.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabNavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
