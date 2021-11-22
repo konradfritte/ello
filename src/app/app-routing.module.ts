94a8240e30ea1575d9049b94d1b41bf47de049e5
Konrad Fritsche <konrad.frit@gmail.com>
2021-11-22 20:39:18 +0100

    master | add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'discover',
+    component: AppComponent
+  },
+  {
+    path: 'likes',
+    component: AppComponent
+  },
+  {
+    path: 'messages',
+    component: AppComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'discover'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
